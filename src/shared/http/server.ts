a94ff2108444cc4e9224fc2e2dac236819cd8743
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-21 23:51:18 -0300

    feat: utitilizando novos middlewares

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,20 @@
-console.log('console.log')
+import 'reflect-metadata';
+import express from 'express';
+import cors from 'cors';
+
+import routes from './routes';
+import errors from './middleware/errors';
+
+import '@shared/typeorm';
+
+const app = express();
+
+app.use(cors);
+app.use(express.json());
+app.use(routes);
+
+app.use(errors);
+
+app.listen('3333', () =>
+  console.log('Server is running at http://localhost:3333 🔥'),
+);
