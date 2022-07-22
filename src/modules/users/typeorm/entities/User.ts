62a4766dc31d9f23995e50d50c95597da20f176d
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-21 23:51:45 -0300

    feat: entidade de usuarios

---
+++ src/modules/users/typeorm/entities/User.ts
@@ -0,0 +1,42 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  organization: string;
+
+  @Column()
+  role: string;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
