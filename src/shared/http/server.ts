ad4c33e156e1d9e46c126bb0d7ef9f056ecf13d0
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-25 20:59:23 -0300

    fix: correcao para o middleware de cors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,20 +1,20 @@
 import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
+import 'express-async-errors';
 
 import routes from './routes';
-import errors from './middleware/errors';
+import errorHandler from './middleware/errors';
 
 import '@shared/typeorm';
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 
-app.use(errors);
-
-app.listen('3333', () =>
-  console.log('Server is running at http://localhost:3333 🔥'),
+app.listen(3001, () =>
+  console.log('🔥 Server is running at http://localhost:3001'),
 );
