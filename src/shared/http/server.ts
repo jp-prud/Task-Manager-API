a28a6d557e849d242b6e1f77c2bf8575705e0828
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-07-25 22:34:15 -0300

    chore: correcao na organizacao de pastas

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 
 import routes from './routes';
 import errorHandler from './middleware/errors';
@@ -13,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(3001, () =>
